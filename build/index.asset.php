51bced07f1e83d82f07589a318e25eb8b922d96a
Chris Yerkes <chris@taylordesign.com>
2021-10-07 16:53:34 -0400

    initial commit

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '586730adf4f98a94d65341ae6fae8981');
